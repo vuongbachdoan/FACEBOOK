25dd6e3b8412f056ab17204e9747ad30bd72366b
Thongtm-0189 <thongminh23102002@gmail.com>
2023-06-22 22:50:23 +0700

    refactor(signin/signup): modify login and signup logic

--- src/pages/auth/signIn/index.jsx
+++ src/pages/auth/signIn/index.jsx
@@ -1,64 +1,101 @@
-import { Form, Button, Schema, ButtonToolbar, Panel } from 'rsuite';
-import FormGroup from 'rsuite/esm/FormGroup';
-import FacebookOfficialIcon from '@rsuite/icons/legacy/FacebookOfficial';
-import GooglePlusCircleIcon from '@rsuite/icons/legacy/GooglePlusCircle';
-import 'bootstrap/dist/css/bootstrap.css';
+import { Form, Button, Schema, ButtonToolbar, Panel } from "rsuite";
+import FormGroup from "rsuite/esm/FormGroup";
+import FacebookOfficialIcon from "@rsuite/icons/legacy/FacebookOfficial";
+import GooglePlusCircleIcon from "@rsuite/icons/legacy/GooglePlusCircle";
+import "bootstrap/dist/css/bootstrap.css";
 const { StringType } = Schema.Types;
 const model = Schema.Model({
   email: StringType()
-    .isEmail('Please enter a valid email address.')
-    .isRequired('Please enter an email address.'),
-  password: StringType().isRequired('Please enter a password.'),
+    .isEmail("Please enter a valid email address.")
+    .isRequired("Please enter an email address."),
+  password: StringType().isRequired("Please enter a password."),
 });
 
 export const SignIn = () => {
-
   const handleSubmit = (formValue) => {
     console.log(formValue);
   };
 
   return (
     <>
-      <Panel header={<h3>Sign in</h3>} bordered className="my-panel col-md-7 shadow">
-  <Form model={model} onSubmit={handleSubmit} className='container row'>
-    <div className='col-md-8 col-sm-12 d-flex flex-column flex-md-row'>
-      <div className='col-md-8 col-sm-12'>
-        <FormGroup className='col-12'>
-          <Form.ControlLabel>Email</Form.ControlLabel>
-          <Form.Control name='email' type='Email' className='p-2'/>
-        </FormGroup>
-        <FormGroup className='col-12'>
-          <Form.ControlLabel>Password</Form.ControlLabel>
-          <Form.Control name='password' type='Password' className='p-2'/>
-        </FormGroup>
-      </div>
-      <FormGroup className='col-md-4 col-sm-12 p-2 m-2'>
-        <ButtonToolbar className='container m-1 d-grid gap-3'>
-          <div className='m-0'></div>
-          <Button appearance='primary'active type='submit' className="my-button btn p-3 shadow">Sign in</Button>
-          <Button appearance='default' className="my-button btn p-2 shadow" >Sign up</Button>
-        </ButtonToolbar>
-      </FormGroup>
-    </div>
-    <div className='col-md-4 col-sm-12'>
-      <Panel className='container shadow' header='Sign in with: ' bordered>
-        <ButtonToolbar className='row'>
-          <Button color="blue" appearance="primary" startIcon={<FacebookOfficialIcon />} className="my-button p-2">
-            Facebook
-          </Button>
-          <Button color="red" appearance="primary" startIcon={<GooglePlusCircleIcon />} className="my-button p-2">
-            Google Plus
-          </Button>
-        </ButtonToolbar>
+      <Panel
+        header={<h3>Sign in</h3>}
+        bordered
+        className="my-panel col-6 shadow row"
+      >
+        <Form
+          model={model}
+          onSubmit={handleSubmit}
+          className="m-2 p-1 col-12 row"
+        >
+          <div className="col-md-8 col-sm-12 row d-flex flex-column flex-md-row">
+            <div className="col-md-6 col-sm-12 m-0">
+              <FormGroup controlId="email" className="col-12">
+                <Form.ControlLabel>Email</Form.ControlLabel>
+                <Form.Control name="email" type="Email" className="p-2 w-100" />
+              </FormGroup>
+              <FormGroup controlId="password" className="col-12">
+                <Form.ControlLabel>Password</Form.ControlLabel>
+                <Form.Control
+                  name="password"
+                  type="Password"
+                  className="p-2 w-100"
+                />
+              </FormGroup>
+            </div>
+            <FormGroup className="col-md-5 col-sm-12">
+              <ButtonToolbar className="m-1 d-grid gap-3">
+                <div className="m-1"></div>
+                <Button
+                  appearance="primary"
+                  active
+                  type="submit"
+                  className="my-button btn p-3 shadow"
+                >
+                  Sign in
+                </Button>
+                <Button
+                  appearance="default"
+                  className="my-button btn p-2 shadow"
+                >
+                  Sign up
+                </Button>
+              </ButtonToolbar>
+            </FormGroup>
+          </div>
+          <div className="col-md-4 col-sm-12">
+            <Panel
+              className="container shadow"
+              header="Sign in with: "
+              bordered
+            >
+              <ButtonToolbar className="row">
+                <Button
+                  color="blue"
+                  appearance="primary"
+                  startIcon={<FacebookOfficialIcon />}
+                  className="my-button p-2"
+                >
+                  Facebook
+                </Button>
+                <Button
+                  color="red"
+                  appearance="primary"
+                  startIcon={<GooglePlusCircleIcon />}
+                  className="my-button p-2"
+                >
+                  Google Plus
+                </Button>
+              </ButtonToolbar>
+            </Panel>
+          </div>
+        </Form>
+        <div className="m-2 col-md-12 col-sm-12">
+          <ButtonToolbar>
+            <Button appearance="link">Forgot password?</Button>
+          </ButtonToolbar>
+        </div>
       </Panel>
-    </div>
-  </Form>
-  <div className='m-2 col-md-12 col-sm-12'>
-        <ButtonToolbar>
-          <Button appearance='link'>Forgot password?</Button>
-        </ButtonToolbar>
-      </div>
-</Panel>
-</>
+    </>
   );
 };
--- src/pages/auth/signUp/index.jsx
+++ src/pages/auth/signUp/index.jsx
@@ -1,29 +1,85 @@
-import { Button, ButtonToolbar, Form } from "rsuite";
+import { Button, ButtonToolbar, Form, Schema } from "rsuite";
+import "bootstrap/dist/css/bootstrap.css";
+
+const { StringType } = Schema.Types;
+const model = Schema.Model({
+  email: StringType()
+    .isEmail("Please enter a valid email address.")
+    .isRequired("Please enter an email address."),
+  password: StringType().isRequired("Please enter a password."),
+  name: StringType().isRequired("Please enter a username."),
+  confirmPassword: StringType()
+    .addRule((value, data) => {
+      if (value !== data.password) {
+        return false;
+      }
+      return true;
+    }, "Password incorrect.")
+    .isRequired("Please confirm password."),
+});
 
 export const SignUp = () => {
-    return (
-        <Form>
-            <h3 className="text-center">Sign Up ✨</h3>
-            <Form.Group controlId="name">
-                <Form.ControlLabel>Username</Form.ControlLabel>
-                <Form.Control name="name" />
-                <Form.HelpText>Username is required</Form.HelpText>
-            </Form.Group>
-            <Form.Group controlId="email">
-                <Form.ControlLabel>Email</Form.ControlLabel>
-                <Form.Control name="email" type="email" />
-                <Form.HelpText tooltip>Email is required</Form.HelpText>
-            </Form.Group>
-            <Form.Group controlId="password">
-                <Form.ControlLabel>Password</Form.ControlLabel>
-                <Form.Control name="password" type="password" autoComplete="off" />
-            </Form.Group>
-            <Form.Group>
-                <ButtonToolbar>
-                    <Button appearance="primary">Ok</Button>
-                    <Button appearance="default">Cancel</Button>
-                </ButtonToolbar>
-            </Form.Group>
-        </Form>
-    );
-}
+  const handleSubmit = (submitValue) => {
+    console.log(submitValue);
+  };
+
+  return (
+    <Form
+      model={model}
+      onSubmit={handleSubmit}
+      className="row col-md-4 col-sm-4 p-2 col-6 d-flex justify-content-center border shadow rounded"
+    >
+      <h3 className="text-center col-md-12 col-sm-12 col-12">Sign Up </h3>
+      <div className="col-md-6 col-sm-12 col-12 container">
+        <Form.Group controlId="name">
+          <Form.ControlLabel>User Name</Form.ControlLabel>
+          <Form.Control
+            className="form-control shadow w-100"
+            type="text"
+            name="name"
+          />
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.ControlLabel>Email</Form.ControlLabel>
+          <Form.Control
+            className="form-control shadow w-100"
+            name="email"
+            type="email"
+          />
+          <Form.HelpText>Email is required</Form.HelpText>
+        </Form.Group>
+      </div>
+      <div className="col-md-6 col-sm-12 col-12">
+        <Form.Group controlId="password">
+          <Form.ControlLabel>Password</Form.ControlLabel>
+          <Form.Control
+            className="form-control shadow w-100"
+            name="password"
+            type="password"
+            autoComplete="off"
+          />
+        </Form.Group>
+        <Form.Group controlId="confirmPassword">
+          <Form.ControlLabel>Confirm Password</Form.ControlLabel>
+          <Form.Control
+            className="form-control shadow w-100"
+            name="confirmPassword"
+            type="password"
+            autoComplete="off"
+          />
+          <Form.HelpText>Please confirm your password.</Form.HelpText>
+        </Form.Group>
+      </div>
+      <div className="col-12 m-2">
+        <Form.Group>
+          <ButtonToolbar>
+            <Button appearance="primary" type="submit">
+              Submit
+            </Button>
+            <Button appearance="default">Sign in</Button>
+          </ButtonToolbar>
+        </Form.Group>
+      </div>
+    </Form>
+  );
+};
